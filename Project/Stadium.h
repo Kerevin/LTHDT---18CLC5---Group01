f7fc6671ec6194e0dd9562a02485c6517c8831f5
Dang Khoa Nguyen <nguyendangkhoa200400@gmail.com>
2019-06-08 17:57:54 +0700

    Update Person Class

---
+++ Project/Stadium.h
@@ -0,0 +1,30 @@
+#pragma once
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class STD //Class Stadium
+{
+private:
+	string m_Name;
+	string m_Address;
+	string m_Kind;
+	double m_Capacity;
+public:
+	//Constructor:
+
+	STD();
+	STD(string name);
+	STD(string name, string add);
+	STD(string name, string add, string kind);
+	STD(string name, string add, string kind, double capacity);
+
+	//Function:
+	void input();
+	void output();
+	void setName(string name);
+	void setAddress(string add);
+	void setKind(string kind);
+	void setCapacity(double capacity);
+};
