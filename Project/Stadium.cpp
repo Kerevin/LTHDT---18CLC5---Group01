129267b06adba4ee736b2975c8fb14df959e4e4f
Duy Long <wlegacy01@gmail.com>
2019-06-09 16:32:52 +0700

    Fix Stadium Class

--- Project/Stadium.cpp
+++ Project/Stadium.cpp
@@ -1,19 +1,12 @@
 #include"Stadium.h"
-<<<<<<< HEAD
 
-=======
->>>>>>> ecd0fd316b0ef9e8955976e47bc6a0999a5b3b2e
 //Constructor:
 STD::STD()
 {
 	m_Name = "";
 	m_Address = "";
 	m_Kind = "";
-<<<<<<< HEAD
 	m_Capacity = 0;
-=======
-	m_Size = 0;
->>>>>>> ecd0fd316b0ef9e8955976e47bc6a0999a5b3b2e
 }
 
 STD::STD(string name)
@@ -34,61 +27,33 @@ STD::STD(string name, string add, string kind)
 	m_Kind = kind;
 }
 
-<<<<<<< HEAD
 STD::STD(string name, string add, string kind, double capacity)
-=======
-STD::STD(string name, string add, string kind, double size)
->>>>>>> ecd0fd316b0ef9e8955976e47bc6a0999a5b3b2e
 {
 	m_Name = name;
 	m_Address = add;
 	m_Kind = kind;
-<<<<<<< HEAD
 	m_Capacity = capacity;
-=======
-	m_Size = size;
->>>>>>> ecd0fd316b0ef9e8955976e47bc6a0999a5b3b2e
 }
 
 //Function:
 void STD::input()
 {
-<<<<<<< HEAD
 	cout << "Enter Stadium's Name: ";
-	cin.ignore();
 	getline(cin, m_Name);
 	cout << "Enter Stadium's Address: ";
-	cin.ignore();
 	getline(cin, m_Address);
 	cout << "Enter Stadium's Kind: ";
-	cin.ignore();
-	getline(cin, m_Address);
+	getline(cin,m_Kind);
 	cout << "Enter Stadium's capacity: ";
 	cin >> m_Capacity;
-=======
-	cin.ignore();
-	cout << "Enter Stadium's Name: ";
-	getline(cin, m_Name);
-	cout << "Enter Stadium's Address: ";
-	getline(cin, m_Address);
-	cout << "Enter Stadium's Kind: ";
-	getline(cin, m_Address);
-	cout << "Enter Stadium's Size: ";
-	cin >> m_Size;
->>>>>>> ecd0fd316b0ef9e8955976e47bc6a0999a5b3b2e
 }
 
 void STD::output()
 {
-	cin.ignore();
 	cout << "Information of " << m_Name << " is: " << endl;
 	cout << "Address: " << m_Address << endl;
 	cout << "Kind: " << m_Kind << endl;
-<<<<<<< HEAD
 	cout << "Capacity: " << m_Capacity << endl;
-=======
-	cout << "Size: " << m_Size << endl;
->>>>>>> ecd0fd316b0ef9e8955976e47bc6a0999a5b3b2e
 }
 
 void STD::setName(string name)
@@ -106,13 +71,7 @@ void STD::setKind(string kind)
 	m_Kind = kind;
 }
 
-<<<<<<< HEAD
 void STD::setCapacity(double capacity)
 {
 	m_Capacity = capacity;
-=======
-void STD::setSize(double size)
-{
-	m_Size = size;
->>>>>>> ecd0fd316b0ef9e8955976e47bc6a0999a5b3b2e
 }
--- Project/Stadium.h
+++ Project/Stadium.h
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-#pragma once
-=======
->>>>>>> ecd0fd316b0ef9e8955976e47bc6a0999a5b3b2e
 #include<iostream>
 #include<string>
 
@@ -13,36 +9,20 @@ private:
 	string m_Name;
 	string m_Address;
 	string m_Kind;
-<<<<<<< HEAD
 	double m_Capacity;
 public:
 	//Constructor:
-
-=======
-	double m_Size;
-public:
-	//Constructor:
->>>>>>> ecd0fd316b0ef9e8955976e47bc6a0999a5b3b2e
 	STD();
 	STD(string name);
 	STD(string name, string add);
 	STD(string name, string add, string kind);
-<<<<<<< HEAD
 	STD(string name, string add, string kind, double capacity);
-
-=======
-	STD(string name, string add, string kind, double size);
->>>>>>> ecd0fd316b0ef9e8955976e47bc6a0999a5b3b2e
 	//Function:
 	void input();
 	void output();
 	void setName(string name);
 	void setAddress(string add);
 	void setKind(string kind);
-<<<<<<< HEAD
 	void setCapacity(double capacity);
-=======
-	void setSize(double size);
 	bool checkQuality();
->>>>>>> ecd0fd316b0ef9e8955976e47bc6a0999a5b3b2e
 };
